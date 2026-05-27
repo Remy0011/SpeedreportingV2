2e680152f37b235334556d316dadf51084081aac
FlorianHeilig <florian.heilig0@gmail.com>
2026-05-27 04:24:40 +0200

    feat: ajout de la page previsionnel

---
+++ src/Views/Planning/index.html.php
@@ -0,0 +1,19 @@
+<?php
+
+use Src\Services\AssetService;
+
+AssetService::addStyle(["_sidebar.css", "_breadcrumb.css", "Button.css", "Modal.css", "_settings.css", "_help.css", "Table.css", "Card.css"]);
+AssetService::addScript(["_sidebar.js", "_breadcrumb.js", "_settings.js", "_help.js", "Table.js"]);
+
+$page_title = "Planning prévisionnel";
+
+require_once __DIR__ . '/../partials/_top.html.php';
+include_once __DIR__ . '/../partials/_sidebar.html.php';
+include_once __DIR__ . '/../partials/_breadcrumb.html.php';
+?>
+
+<section class="content">
+    <h1>Planning prévisionnel</h1>
+</section>
+
+<?php require_once __DIR__ . '/../partials/_bottom.html.php'; ?>
