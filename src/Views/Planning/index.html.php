533004f6896aaf602945909ee6b177cca0dff78c
Bruno AMUZU <amuzuelbruno@gmail.com>
2026-06-03 00:09:02 +0200

    index

--- src/Views/Planning/index.html.php
+++ src/Views/Planning/index.html.php
@@ -2,7 +2,9 @@
 
 use Src\Services\AssetService;
 
-AssetService::addStyle(["_sidebar.css", "_breadcrumb.css", "Button.css", "Modal.css", "_settings.css", "_help.css", "Table.css", "Card.css"]);
+$view = $_GET['view'] ?? 'project'; // Projets par défaut
+
+AssetService::addStyle(["_sidebar.css", "_breadcrumb.css", "Button.css", "Modal.css", "_settings.css", "_help.css", "Table.css", "Card.css", "_toolbar.css"]);
 AssetService::addScript(["_sidebar.js", "_breadcrumb.js", "_settings.js", "_help.js", "Table.js"]);
 
 $page_title = "Planning prévisionnel";
@@ -14,6 +16,17 @@ include_once __DIR__ . '/../partials/_breadcrumb.html.php';
 
 <section class="content">
     <h1>Planning prévisionnel</h1>
+    <?php include_once __DIR__ . '/../partials/cards/_toolbar.html.php'; ?>
+</section>
+
+<section class="content">
+    <?php
+    match($view) {
+        'users'    => include_once __DIR__ . '/user_vue.html.php',
+    'calendar' => include_once __DIR__ . '/calendar_vue.html.php',
+    default    => include_once __DIR__ . '/project_vue.html.php',
+    };
+    ?>
 </section>
 
 <?php require_once __DIR__ . '/../partials/_bottom.html.php'; ?>
