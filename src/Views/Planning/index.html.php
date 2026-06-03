cdcabe71336315629fee7687753046676f8c67c7
Remy0011 <remy.deiber1999@gmail.com>
2026-06-03 16:18:08 +0200

    feat: calendar

--- src/Views/Planning/index.html.php
+++ src/Views/Planning/index.html.php
@@ -2,8 +2,8 @@
 
 use Src\Services\AssetService;
 
-AssetService::addStyle(["_sidebar.css", "_breadcrumb.css", "Button.css", "Modal.css", "_settings.css", "_help.css", "Table.css", "Card.css"]);
-AssetService::addScript(["_sidebar.js", "_breadcrumb.js", "_settings.js", "_help.js", "Table.js"]);
+AssetService::addStyle(["_sidebar.css", "_breadcrumb.css", "Button.css", "Modal.css", "_settings.css", "_help.css", "Calendar.css", "Card.css", "Planning.css"]);
+AssetService::addScript(["_sidebar.js", "_breadcrumb.js", "_settings.js", "_help.js"]);
 
 $page_title = "Planning prévisionnel";
 
@@ -14,6 +14,7 @@ include_once __DIR__ . '/../partials/_breadcrumb.html.php';
 
 <section class="content">
     <h1>Planning prévisionnel</h1>
+    <?php include_once __DIR__ . '/week_calendar.html.php'; ?>
 </section>
 
 <?php require_once __DIR__ . '/../partials/_bottom.html.php'; ?>
