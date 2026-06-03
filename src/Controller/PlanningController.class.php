58d4c2f1ca63d253a7b5cbff42f7087fe75ee742
Remy0011 <remy.deiber1999@gmail.com>
2026-06-03 14:43:18 +0200

    feat: planningcontroller

--- src/Controller/PlanningController.class.php
+++ src/Controller/PlanningController.class.php
@@ -6,6 +6,6 @@ class PlanningController extends BaseController
 {
     public function getIndex(): void
     {
-        $this::render('Planning/index', []);
+        (new WorkController())->getSelf();
     }
 }
